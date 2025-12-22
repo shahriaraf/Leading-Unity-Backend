c1eb1ef8db178867e429a8aa2bc2ca67f9035fff
Shoumo Shahriar Araf <shahriaraf01@gmail.com>
2025-12-22 18:37:35 +0600

    fixed

--- server.js
+++ server.js
@@ -37,11 +37,11 @@ app.use(notFound);
 app.use(errorHandler);
 
 // ✅ UPDATED: Works on Vercel AND Localhost
-// if (require.main === module) {
-//   const PORT = process.env.PORT || 5000;
-//   app.listen(PORT, () => {
-//     console.log(`Server running locally on port ${PORT}`);
-//   });
-// }
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running locally on port ${PORT}`);
+  });
+}
 
 module.exports = app;
